a9f72cc755b9f1dc6e6e5ecb6b5e9c9aa07bfb83
joe <josulliv101@gmail.com>
2024-02-18 13:14:14 -0500

    push images

--- src/app/data.ts
+++ src/app/data.ts
@@ -35,7 +35,7 @@ export const projects = [
     title: "ButcherBox",
     content:
       "I worked on the team responsible for the checkout flow forButcherBox.com's storefront - it's powered by Shopify/Hydrogen and integratedwith Contentful. The project involved migrating the old storefront to useRemix.js. I contributed to both the migration and addressed bugs/new features onthe original storefont.",
-    photoUrl: "https://portfolio-a3218.web.app/butcherbox-526x381-min.png",
+    photoUrl: "/butcherbox-526x381-min.png",
     tags: ["react", "typescript", "remix", "graphql"],
     callToAction: {
       label: "Visit site",
@@ -47,7 +47,7 @@ export const projects = [
     title: "Instrument, LLC",
     content:
       "Iworked on a team involved in creating a component libary using chakra-ui as thefoundation, then assembled these components into pages to meet thedesign/functionality requirements of the client. The thumbnail is one of manyassembled pages - this one involves allowing students to be able to create acustom avatar of themselves.",
-    photoUrl: "https://portfolio-a3218.web.app/gg-550x341-min.png",
+    photoUrl: "/gg-550x341-min.png",
     tags: ["react", "typescript", "chakra-ui"],
     callToAction: {
       label: "Not avalable to public",
@@ -58,7 +58,7 @@ export const projects = [
     id: "weld-north",
     title: "Weld North Analytics",
     content: `I wasa member of a 2-person frontend team that created an embeddable React app thatallows educators to create online courses for students. The thumbnail shows thestudent view of a lesson within a course. Functionality for students includesadding annotations to course text/images, dictionary lookups of selected text,audio & video playback, and comprehension questions at the bottom of eachlesson. Educators can create & assemble different content types (richtext,images, videos, audio, questions) into a multi-page course, use a drag/dropinterface to update layout/content placement, leverage other educators' sharedcourse content, and view analytics data (mostly relating to question-responsebreakdown and video watching stats). Note: the product can be seen in actionfrom a student's perspective at 1 minute 58 seconds into the linked video.`,
-    photoUrl: "https://portfolio-a3218.web.app/imaginelearning-550x351-min.png",
+    photoUrl: "/imaginelearning-550x351-min.png",
     tags: ["react", "redux", "graphql"],
     callToAction: {
       label: "Visit site",
@@ -69,7 +69,7 @@ export const projects = [
     id: "hmh",
     title: "Houghton Mifflin Harcourt",
     content: `This project involved working on a team to convert legacy Flash appsrelating to online learning for young students over to React applications. I wasone of three React resources brought in to help guide the Engineering teamthrough their first dive into React application development as well as help inthe conversions.`,
-    photoUrl: "https://portfolio-a3218.web.app/hmh-550x336-min.png",
+    photoUrl: "/hmh-550x336-min.png",
     tags: ["react", "redux", "redux-saga"],
     callToAction: {
       label: "Not avalable to public",
@@ -80,7 +80,7 @@ export const projects = [
     id: "tufts",
     title: "Tufts University - Campus Maps",
     content: `This isan oldie but a goodie - created way back in 2012, but it is still being activelyused. I created a reusable map component to power all 3 of Tufts University'scampus maps. I also helped support the main www.tufts.edu & now.tufts.eduwebsites.`,
-    photoUrl: "https://portfolio-a3218.web.app/tuftscampusmap-550x351-min.png",
+    photoUrl: "/tuftscampusmap-550x351-min.png",
     tags: ["google-maps-api"],
     callToAction: {
       label: "Visit site",
@@ -91,7 +91,7 @@ export const projects = [
     id: "personal-capital",
     title: "Personal Capital",
     content: `This project involvedacting as support on the Empower 401k site. It involved debugging & issueresolution for the application.`,
-    photoUrl: "https://portfolio-a3218.web.app/empowerretirement-min.png",
+    photoUrl: "/empowerretirement-min.png",
     tags: ["react", "redux", "typescript"],
     callToAction: {
       label: "Not avalable to public",
@@ -102,7 +102,7 @@ export const projects = [
     id: "pigpile",
     title: "Pigpile Corporation",
     content: `This is aversion of an online fundraising application I created for Pigpile Corporation afew years back. The functionality basically mirrored that of the GoFundMeapplication but with a different business model.`,
-    photoUrl: "https://portfolio-a3218.web.app/pigpile550x337-min.png",
+    photoUrl: "/pigpile550x337-min.png",
     tags: ["react", "redux", "typescript", "nextjs"],
     callToAction: {
       label: "No longer active",
